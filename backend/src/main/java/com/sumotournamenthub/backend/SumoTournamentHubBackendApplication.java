575e16ea1ccdf414bb57c1fef962a3bfc9c38a95
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-09 12:49:22 +0100

    Init project

---
+++ backend/src/main/java/com/sumotournamenthub/backend/SumoTournamentHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sumotournamenthub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumoTournamentHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumoTournamentHubBackendApplication.class, args);
+	}
+
+}
