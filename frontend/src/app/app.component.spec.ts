e983e38085410add37c0a5d6087ca325f67e2e25
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-14 15:27:44 +0100

    [KT-70] Add navigation

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('sumo-tournament-hub-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sumo-tournament-hub-frontend');
-  });
 });
