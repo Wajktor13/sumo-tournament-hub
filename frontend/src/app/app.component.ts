31ac29279e21bb39b026bc0932e27ad4935ef6cd
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-15 09:55:13 +0100

    [KT-95] Add modules to components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, AppModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
