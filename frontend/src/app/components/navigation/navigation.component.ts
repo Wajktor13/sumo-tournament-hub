96ab52f5f9a6e15ba9d6eb7d744dcc04676d0fe8
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-15 09:55:13 +0100

    [KT-95] Add modules to components

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
