2a65694a975e28be31d25fb0a0d0ca8a93cec5e1
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-15 09:57:52 +0100

    [KT-95] Fix tests

--- frontend/src/app/components/page-not-found/page-not-found.component.spec.ts
+++ frontend/src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { PageNotFoundModule } from './page-not-found.module';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -8,7 +9,7 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageNotFoundComponent],
+      imports: [PageNotFoundModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PageNotFoundComponent);
