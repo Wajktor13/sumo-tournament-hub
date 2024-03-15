9ee3d6f0fcfcd76f255356b47764962b9485480f
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-15 10:00:01 +0100

    [KT-95] Remove unused import

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { AppModule } from './app.module';
 
 @Component({
