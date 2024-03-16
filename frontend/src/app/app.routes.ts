124acda667ff4acda69ebd2b6f1adb25a8a32e29
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-16 12:40:40 +0100

    [KT-50] Add login component and service

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
