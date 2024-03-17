f16d460c5f63666b2726e9d6c5e4c5f5d9c9b6ac
Michał Kuszewski <michal.kuszewski09@gmail.com>
2024-03-17 15:43:34 +0100

    Marked Category and Season classes as entities and added them primary keys

--- backend/src/main/java/com/sumotournamenthub/backend/season/Season.java
+++ backend/src/main/java/com/sumotournamenthub/backend/season/Season.java
@@ -1,5 +1,13 @@
 package com.sumotournamenthub.backend.season;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Season
 {
+    @Id
+    @GeneratedValue
+    private int id;
 }
