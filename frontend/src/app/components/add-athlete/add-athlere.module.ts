1c494bda2ce59f603ea3fdd73f3eaa5f71e8b480
Tomasz <87528104+T4oMeK@users.noreply.github.com>
2024-03-18 14:00:24 +0100

    Added form to add an athlete.

---
+++ frontend/src/app/components/add-athlete/add-athlere.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddAthleteComponent } from './add-athlete.component';
+
+@NgModule({
+  declarations: [AddAthleteComponent],
+  imports: [CommonModule],
+  exports: [AddAthleteComponent],
+})
+export class AddAthleteModule {}
