047f2ba67fc3dfddca64df9f5ca7f1e896af1fa9
Michał Kuszewski <michal.kuszewski09@gmail.com>
2024-03-18 17:14:00 +0100

    Merged with develop, added blank lines for readability, created limits for participating athletes for each country

--- backend/src/main/java/com/sumotournamenthub/backend/competition/Competition.java
+++ backend/src/main/java/com/sumotournamenthub/backend/competition/Competition.java
@@ -1,8 +1,9 @@
 package com.sumotournamenthub.backend.competition;
 
 import com.sumotournamenthub.backend.athlete.Athlete;
-import com.sumotournamenthub.backend.category.Category;
-import com.sumotournamenthub.backend.season.Season;
+import com.sumotournamenthub.backend.constants.Country;
+import com.sumotournamenthub.backend.domain.AgeCategory;
+import com.sumotournamenthub.backend.domain.Season;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NonNull;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Set;
 
 @Entity
@@ -22,15 +24,24 @@ public class Competition
     @Id
     @GeneratedValue
     private int id;
+
     private @NonNull String name;
+
     @ManyToOne
     private @NonNull Season season;
+
     private @NonNull LocalDate startTime;
+
     private @NonNull LocalDate endTime;
 
     @OneToMany
     private @NonNull Set<Athlete> signedAthletes;
-    private int maxAthletesPerCountry;
+
+    @ElementCollection
+    @MapKeyEnumerated(EnumType.STRING)
+    private Map<Country, Integer> countryLimits;
+
     @OneToMany
-    private @NonNull Set<Category> availableCategories;
+    private @NonNull Set<AgeCategory> availableCategories;
+
 }
