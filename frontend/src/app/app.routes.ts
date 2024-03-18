994192b9525d84f55e3f5b655b279c44c9856bda
MatoKnap <mknap@student.agh.edu.pl>
2024-03-18 16:39:28 +0100

    Added create competiotion form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddCompetitionComponent } from './components/add-competition/add-competition.component';
 
 export const routes: Routes = [
   { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: 'add-competition', pathMatch: 'full', component: AddCompetitionComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
