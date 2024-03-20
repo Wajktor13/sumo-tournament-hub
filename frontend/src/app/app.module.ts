cb75b184f4f86e1fed32fb20e148a7bc2b53368b
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-20 14:57:43 +0100

    [KT-99] Add basic auth logic

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationModule } from './components/navigation/navigation.module';
 import { AddSeasonModule } from './components/add-season/add-season.module';
+import { LoginModule } from './components/login/login.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NavigationModule, AddSeasonModule],
+  imports: [CommonModule, NavigationModule, AddSeasonModule, LoginModule],
   exports: [NavigationModule],
 })
 export class AppModule {}
