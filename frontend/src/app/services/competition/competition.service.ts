a9ab761c02c29d557c676ac5bfa006ebe5ea50fa
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-21 00:25:34 +0100

    [KT-100] Add Competition model and service with mocked data

---
+++ frontend/src/app/services/competition/competition.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Competition } from '../../models/competition';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompetitionService {
+  constructor() {}
+
+  public getAll(): Observable<Competition[]> {
+    return of([
+      {
+        id: 1,
+        name: 'Mock Competition 1',
+        startTime: new Date(),
+        endTime: new Date(),
+        countryLimits: { USA: 10, UK: 15 },
+      },
+      {
+        id: 2,
+        name: 'Mock Competition 2',
+        startTime: new Date(),
+        endTime: new Date(),
+        countryLimits: { USA: 20, UK: 25 },
+      },
+    ]);
+  }
+
+  public getOne(id: number): Observable<Competition | undefined> {
+    return of({
+      id: id,
+      name: 'Mock Competition 1',
+      startTime: new Date(),
+      endTime: new Date(),
+      countryLimits: { USA: 10, UK: 15 },
+    });
+  }
+}
