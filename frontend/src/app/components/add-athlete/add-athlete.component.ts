d1abf3ad9843e1a9feb56a24b3d568ae7dc03527
Tomasz <87528104+T4oMeK@users.noreply.github.com>
2024-03-21 14:57:04 +0100

    Added form validation for adding athlete

--- frontend/src/app/components/add-athlete/add-athlete.component.ts
+++ frontend/src/app/components/add-athlete/add-athlete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-athlete',
@@ -6,6 +8,34 @@ import { Component } from '@angular/core';
   templateUrl: './add-athlete.component.html',
   styleUrl: './add-athlete.component.css'
 })
-export class AddAthleteComponent {
+export class AddAthleteComponent implements OnInit {
+  date?: string;
 
+  ngOnInit() {
+    this.date = new Date().toISOString().slice(0, 10);
+  }
+
+  addAthleteForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\p{L}\s\-.']+$/u)
+    ]),
+    secondName: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[\p{L}\s\-.']+$/u)
+    ]),
+    club: new FormControl('', [
+      Validators.required
+    ]),
+    gender: new FormControl('', [
+      Validators.required
+    ]),
+    birthDate: new FormControl('', [
+      Validators.required
+    ])
+  })
+
+  submitForm() {
+    console.log(this.addAthleteForm.valid);
+  }
 }
