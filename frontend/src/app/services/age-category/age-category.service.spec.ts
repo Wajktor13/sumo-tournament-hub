436ca005aac1a6076f5555a569f58cb2bd48d806
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-22 23:31:44 +0100

    [KT-118] Connect age category service with backend

--- frontend/src/app/services/age-category/age-category.service.spec.ts
+++ frontend/src/app/services/age-category/age-category.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AgeCategoryService } from './age-category.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AgeCategoryService', () => {
   let service: AgeCategoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(AgeCategoryService);
   });
 
