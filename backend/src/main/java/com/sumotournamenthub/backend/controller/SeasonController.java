1cabf5e2fc09dd8a9061ad4216d352bc0da8f2da
Michał Kuszewski <michal.kuszewski09@gmail.com>
2024-03-22 14:38:53 +0100

    Moved persistence methods to SeasonService, merged with develop

--- backend/src/main/java/com/sumotournamenthub/backend/controller/SeasonController.java
+++ backend/src/main/java/com/sumotournamenthub/backend/controller/SeasonController.java
@@ -1,13 +1,15 @@
 package com.sumotournamenthub.backend.controller;
 
 import com.sumotournamenthub.backend.domain.AgeCategory;
+import com.sumotournamenthub.backend.dto.SeasonDto;
 import com.sumotournamenthub.backend.repository.SeasonRepository;
+import com.sumotournamenthub.backend.service.SeasonService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static com.sumotournamenthub.backend.utils.ExceptionUtils.notExist;
@@ -19,6 +21,8 @@ public class SeasonController {
 
     private final SeasonRepository repository;
 
+    private SeasonService seasonService;
+
     public SeasonController(SeasonRepository repository) {
         this.repository = repository;
     }
@@ -29,4 +33,60 @@ public class SeasonController {
         return ResponseEntity.ok(season.getCategories());
     }
 
+    @GetMapping
+    public List<SeasonDto> getAllSeasons()
+    {
+        return seasonService.getAllSeasons();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeasonDto> getSeasonById(@PathVariable int id)
+    {
+        Optional<SeasonDto> seasonDto = seasonService.getSeasonById(id);
+
+        if (seasonDto.isPresent())
+        {
+            return ResponseEntity.ok(seasonDto.get());
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SeasonDto> updateSeason(@PathVariable int id, @RequestBody SeasonDto seasonDto)
+    {
+        Optional<SeasonDto> seasonResponse = seasonService.updateSeason(id, seasonDto);
+
+        if (seasonResponse.isPresent())
+        {
+            return ResponseEntity.ok(seasonResponse.get());
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<SeasonDto> createSeason(@RequestBody SeasonDto seasonDto)
+    {
+        SeasonDto seasonResponse = seasonService.createSeason(seasonDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(seasonResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeason(@PathVariable int id)
+    {
+        if (seasonService.deleteSeason(id))
+        {
+            return ResponseEntity.noContent().build();
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
