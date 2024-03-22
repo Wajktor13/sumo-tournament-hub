d77d1fba7c1fae79fcf66259ad3fdc56adcd8196
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-22 14:18:35 +0100

    [KT-116] Add no args constructor

--- backend/src/main/java/com/sumotournamenthub/backend/domain/Club.java
+++ backend/src/main/java/com/sumotournamenthub/backend/domain/Club.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
@@ -13,6 +14,7 @@ import org.springframework.lang.NonNull;
 @Entity
 @Table(name = "club")
 @RequiredArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class Club {
 
