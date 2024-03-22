157a34fe8e1b42fa3a9c115d7dc157bbedb79f34
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-03-22 23:58:12 +0100

    [KT-117] Connect season service with backend

--- frontend/src/app/services/season/season.service.spec.ts
+++ frontend/src/app/services/season/season.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SeasonService } from './season.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SeasonService', () => {
   let service: SeasonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(SeasonService);
   });
 
