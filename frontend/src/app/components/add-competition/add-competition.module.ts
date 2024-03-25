75e19719149bdcdbda2863762da9c67ef69414dd
MatoKnap <mknap@student.agh.edu.pl>
2024-03-25 22:04:46 +0100

    Added partial competition validation

--- frontend/src/app/components/add-competition/add-competition.module.ts
+++ frontend/src/app/components/add-competition/add-competition.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddCompetitionComponent } from './add-competition.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddCompetitionComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [AddCompetitionComponent],
 })
 export class AddCompetitionModule {}
