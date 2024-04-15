1c7aad7998a72ec37199e437ecb66116160617b9
Tomasz <87528104+T4oMeK@users.noreply.github.com>
2024-04-15 21:05:01 +0200

    Removed parameters of IsSignedInGuard

--- frontend/src/app/services/auth/is-signed-in.guard.ts
+++ frontend/src/app/services/auth/is-signed-in.guard.ts
@@ -1,12 +1,9 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
-export const IsSignedInGuard: CanActivateFn = (
-  _route: ActivatedRouteSnapshot,
-  _state: RouterStateSnapshot
-):
+export const IsSignedInGuard: CanActivateFn = ():
   Observable<boolean | UrlTree> 
   | Promise<boolean | UrlTree> 
   | boolean 
